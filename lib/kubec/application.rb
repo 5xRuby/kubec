1533b5e00db0b8e36ce89af1fc155bd526e19de7
蒼時弦也 <elct9620@frost.tw>
2018-01-08 16:47:50 +0800

    Disable unused options

--- lib/kubec/application.rb
+++ lib/kubec/application.rb
@@ -22,8 +22,45 @@ module Kubec
       super
     end
 
+    def handle_options
+      options.rakelib = ['rakelib']
+      options.trace_output = STDERR
+
+      OptionParser.new do |opts|
+        opts.on_tail('-h', '--help', '-H', 'Display this help message.') do
+          puts opts
+          exit
+        end
+
+        standard_rake_options.each { |args| opts.on(*args) }
+        opts.environment('KUBEOPT')
+      end.parse!
+    end
+
+    def sort_options(options)
+      supported_options = %w[tasks]
+
+      options.select! do |(switch, *)|
+        switch =~ /--#{Regexp.union(supported_options)}/
+      end
+
+      super.push(version)
+    end
+
     def default_kubeconfig
       File.expand_path('../../Kubeconfig', __FILE__)
+    end
+
+    def version
+      [
+        '--version', '-V',
+        'Display the program version.',
+        lambda do |_value|
+          puts "Kubec Version: #{Kubec::VERSION} "\
+               "(Rake Version: #{Rake::VERSION})"
+          exit
+        end
+      ]
     end
   end
 end
