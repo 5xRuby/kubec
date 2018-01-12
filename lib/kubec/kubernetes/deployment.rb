73c058fe75b5d3dee7e0ddf550d10125998acf5a
蒼時弦也 <elct9620@frost.tw>
2018-01-12 18:34:03 +0800

    Add template selector support

--- lib/kubec/kubernetes/deployment.rb
+++ lib/kubec/kubernetes/deployment.rb
@@ -12,6 +12,19 @@ module Kubec
         spec[:template] ||= Template.new
         spec[:template].instance_eval(&block)
       end
+
+      # TODO: Move into module
+      def select(key, value)
+        spec[:selector] ||= { matchLabels: {} }
+        spec[:selector][:matchLabels][key] = value
+      end
+
+      def selector=(labels)
+        # TODO: Check labels type
+        spec[:selector] = labels
+      end
+
+      alias selector selector=
     end
   end
 end
